a61fc02e355308efd3ee84d2c9980c4127bad0c1
dmitriygalyanov <dimagalyanov@gmail.com>
2022-07-21 01:55:55 +1000

    Sphere Anim
    add shared/commonStyles

---
+++ src/shared/commonStyles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  screenWrap: { flex: 1, backgroundColor: 'gray' },
+
+  flex1: { flex: 1 },
+
+  fillParent: { width: '100%', height: '100%' },
+});
+
+export const buttonTextColor = 'black';
