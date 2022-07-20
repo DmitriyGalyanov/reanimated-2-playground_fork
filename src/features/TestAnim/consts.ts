d9b87f23cbb77c9e9e9c768dc768ee7b04076aaf
dmitriygalyanov <dimagalyanov@gmail.com>
2022-07-20 18:26:36 +1000

    Sphere Anim
    add MAIN_SPHERE_COMPRESSED_SCALE
        MAIN_SPHERE_EXPANDED_SCALE
        MAIN_SPHERE_ANIM_EASING
        ViewLayersEnum

--- src/features/TestAnim/consts.ts
+++ src/features/TestAnim/consts.ts
@@ -1,6 +1,21 @@
-import { Dimensions } from 'react-native';
+import { Easing } from 'react-native-reanimated';
 
-export const { height: WINDOW_HEIGHT } = Dimensions.get('window');
 export const INIT_SPHERE_SIZE = 40;
+export const MAIN_SPHERE_COMPRESSED_SCALE = 1;
+export const MAIN_SPHERE_EXPANDED_SCALE = 5;
+
 export const DEFAULT_SINGLE_SQUEEZE_ANIM_DURATION = 100;
 export const DEFAULT_MAIN_SPHERE_ANIM_DURATION = 5000;
+export const MAIN_SPHERE_ANIM_EASING = Easing.quad;
+
+/**
+ * { zIndex: ViewLayersEnum.second } overlaps { zIndex: ViewLayersEnum.fourth }
+ */
+export enum ViewLayersEnum {
+  'topMost' = 1000,
+  'first' = 50,
+  'second' = 40,
+  'third' = 30,
+  'fourth' = 20,
+  'fifth' = 10,
+}
