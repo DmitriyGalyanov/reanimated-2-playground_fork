4c6cb7e4ff987d01a710d645c35eb08aba1621ca
dmitriygalyanov <dimagalyanov@gmail.com>
2022-07-20 18:31:26 +1000

    Sphere Anim
    update SphereImage

--- src/features/TestAnim/SphereImage.tsx
+++ src/features/TestAnim/SphereImage.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
-import { ImageStyle, StyleProp } from 'react-native';
+import { ImageProps, ImageStyle, StyleProp } from 'react-native';
 import Animated from 'react-native-reanimated';
 
 import sphereImg from './assets/sphere.png';
 
-interface ISphereImageProps {
+interface ISphereImageProps extends Omit<ImageProps, 'source' | 'style'> {
   style: StyleProp<Omit<ImageStyle, 'width' | 'height'>>;
   width: number;
   height: number;
 }
 
-const SphereImage: React.FC<ISphereImageProps> = ({ style, width, height }) => {
+const SphereImage: React.FC<ISphereImageProps> = ({
+  style,
+  width,
+  height,
+  ...props
+}) => {
   return (
-    <Animated.Image source={sphereImg} style={[style, { width, height }]} />
+    <Animated.Image
+      source={sphereImg}
+      style={[style, { width, height }]}
+      {...props}
+    />
   );
 };
 
