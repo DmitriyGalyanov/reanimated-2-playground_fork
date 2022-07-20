5d1337f7a49999ccc9528f8a82d00b81d4d3946a
dmitriygalyanov <dimagalyanov@gmail.com>
2022-07-20 16:45:23 +1000

    Sphere Anim
    add TestAnim consts

---
+++ src/features/TestAnim/consts.ts
@@ -0,0 +1,6 @@
+import { Dimensions } from 'react-native';
+
+export const { height: WINDOW_HEIGHT } = Dimensions.get('window');
+export const INIT_SPHERE_SIZE = 40;
+export const DEFAULT_SINGLE_SQUEEZE_ANIM_DURATION = 100;
+export const DEFAULT_MAIN_SPHERE_ANIM_DURATION = 5000;
