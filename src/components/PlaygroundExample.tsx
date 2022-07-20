85503dd5d4c6d4ea9c6706b3f67a0ec49e8b48e1
dmitriygalyanov <dimagalyanov@gmail.com>
2022-07-20 16:42:19 +1000

    Sphere Anim
    update react-native-gesture-handler

--- src/components/PlaygroundExample.tsx
+++ src/components/PlaygroundExample.tsx
@@ -10,6 +10,7 @@ import { View, Button } from 'react-native';
 export default function PlaygroundExample() {
   const randomWidth = useSharedValue(10);
 
+  console.log('render');
   const config = {
     duration: 500,
     easing: Easing.bezier(0.5, 0.01, 0, 1),
@@ -24,15 +25,18 @@ export default function PlaygroundExample() {
   return (
     <View
       style={{
-        flex: 1,
+        // flex: 1,
         flexDirection: 'column',
         backgroundColor: 'yellow',
       }}
     >
-      <Animated.View style={[{ width: 100, height: 80 }, style]} />
+      <Animated.View
+        style={[{ width: 100, height: 80, backgroundColor: 'black' }, style]}
+      />
       <Button
         title="toggle"
         onPress={() => {
+          console.log('press');
           randomWidth.value = Math.random() * 350;
         }}
       />
