924c1107d6b3c1233eb3f3de2e8312b25de7895d
dmitriygalyanov <dimagalyanov@gmail.com>
2022-07-20 16:51:29 +1000

    Sphere Anim
    add useScaleAnim

---
+++ src/features/TestAnim/hooks/useScaleAnim.ts
@@ -0,0 +1,35 @@
+import {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
+import { useCallback } from 'react';
+import { DEFAULT_MAIN_SPHERE_ANIM_DURATION } from 'features/TestAnim/consts';
+
+const useScaleAnim = () => {
+  const scale = useSharedValue(1);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ scale: scale.value }],
+  }));
+
+  const animateScale = useCallback(
+    ({
+      toScale,
+      duration = DEFAULT_MAIN_SPHERE_ANIM_DURATION,
+    }: {
+      toScale: number;
+      duration?: number;
+    }) => {
+      scale.value = withTiming(toScale, { duration });
+    },
+    [scale],
+  );
+
+  return {
+    animatedStyle,
+    animateScale,
+  };
+};
+
+export default useScaleAnim;
